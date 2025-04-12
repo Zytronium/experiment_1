7c7f7cd6d9a4d2195a357b3a9920a333043cbda9
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2025-04-12 15:28:26 -0500

    Prepared for test 6 by creating a JSON file and correctly parsing it while fetching the local file.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
     const testContainer = document.getElementById("test-container");
 
-    fetch('https://thefactionnexus.tech/assets/scripts/index.js')
-        .then((response) => response.text())
-        .then((text) => {
-            testContainer.innerHTML = text;
+    fetch('testJson.json')
+        .then((response) => response.json())
+        .then((data) => {
+            const ul = document.createElement('ul');
+
+            data.forEach(item => {
+                const li = document.createElement('li');
+                li.textContent = item.text;
+                li.style.color = item.color;
+                ul.appendChild(li);
+            });
+
+            testContainer.appendChild(ul);
         })
         .catch((error) => {
             console.error(error);
-            testContainer.innerHTML = error.message;
-        })
+            testContainer.textContent = error.message;
+        });
 });
